bf68dc8d55a86233e73c9a93c18ba995d8f107f3
yuanda <yuandahe1@gmail.com>
2014-02-22 23:20:59 -0500

    Added gems google_drive activerecord-tableless

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,10 @@
 class VisitorsController < ApplicationController
 
   def new
+      flash[:notice] = 'Welcome!'
+      flash[:alert] = 'Access not allowed.'
+      flash[:warning]='Warning'
+      flash[:info]='Info'
   end
 
 end
